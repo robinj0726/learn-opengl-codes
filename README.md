d3b3a1abd3b78195fed0583f5fc4e5a0f272faa8
robinj730 <88188467+robinj730@users.noreply.github.com>
2024-05-11 22:50:36 +0800

    setup the env

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # learn-opengl-codes
+
+- [Learn OpenGL](https://learnopengl-cn.github.io/)
+- [Learn OpenGL CN](https://learnopengl.com/Getting-started/OpenGL)
+
+## Build on Windows
+
+```console
+meson build --backend=vs
+```
+
+For the subproject glew32, please change the value of the compilation option of Basic Runtime Checks to "Defalt" in MSVC
---
+++ hello_window.cpp
@@ -0,0 +1,61 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Create a GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello Window", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Set the viewport size
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Process events
+        glfwPollEvents();
+
+        // Render here
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+    return 0;
+}
